4df99fc3679ee81494d83452167f15c6cb0dccc0
Hafiz Irfan <hafizirfan8078@gmail.com>
2026-04-23 00:03:44 +0500

    show fifa content in pdf

---
+++ resources/views/pdfs/partials/booking-pdf-terms-fifa-and-standard.blade.php
@@ -0,0 +1,4 @@
+{{-- Deprecated name: legacy → FIFA addendum → thank you. --}}
+@include('pdfs.partials.legacy-cancellation-policy-pdf')
+@include('pdfs.partials.fifa-2026-event-policy-pdf')
+@include('pdfs.partials.booking-pdf-thank-you-closing')
